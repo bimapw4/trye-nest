2ff20f83fa2a1b4244265e12fb7e6278cc0f94d0
bimapw4 <bimapw4@gmail.com>
2020-09-04 10:54:00 +0700

    crud

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -10,5 +10,4 @@ export class UserEntity {
 
     @Column()
     ttl: Date
-    
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private user: UsersService) {}
+    
+    // @Post()
+    // create(@Body() body) {
+    //     return this.user.createUser(body)
+    // }
+
+    @Get()
+    findAll() {
+        return this.user.getUser()
+    }
+
+    // @Put(':id')
+    // findParams(@Param('id') id, @Body() data){
+    //     data.id = id
+    //     return this.user.updateUser(data)
+    // }
+
+    // @Delete()
+    // delete(@Query('id') id){
+    //     return this.user.deleteUser(id)
+    // }
+}
